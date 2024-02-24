5dee19cbeda91288b375a29ce3238da5323cfa1d
Ezra Erewharighe <kelvinerewharighe@gmail.com>
2024-02-24 04:01:11 +0100

    Updated..

--- components/SideNavbar.tsx
+++ components/SideNavbar.tsx
@@ -1,22 +1,35 @@
 "use client"
 
-import React from 'react'
+import React, { useState } from 'react'
 
 import {
+    ChevronRight,
     LayoutDashboard,
     Settings,
     ShoppingCart,
     UsersRound,
   } from "lucide-react"
 import { Nav } from './ui/nav'
+import { Button } from './ui/button'
 
 type Props = {}
 
 export default function SideNavbar({}: Props) {
+    const [isCollapsed, setIsCollapsed] = useState(false);
+
+    function toggleSidebar(){
+        setIsCollapsed(!isCollapsed);
+    }
+
   return (
-    <div>
+    <div className='relative min-w-[80px] border-r px-3 pb-10 pt-24'>
+        <div className="absolute right-[-20px] top-7">
+            <Button onClick={toggleSidebar} variant="secondary" className='rounded-full p-2'>
+                <ChevronRight/>
+            </Button>
+        </div>
         <Nav
-            isCollapsed={false}
+            isCollapsed={isCollapsed}
             links={[
               {
                 title: "Dashboard",
