eac01ef0162ca9feaaf6924f56489f40dc55d28c
Ezra Erewharighe <kelvinerewharighe@gmail.com>
2024-02-24 04:55:50 +0100

    Updated...

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,34 @@
+import { CardProps } from "@/components/Card";
 import PageTitle from "@/components/PageTitle";
+import { Activity, CreditCard, DollarSign, Users } from "lucide-react";
 import Image from "next/image";
+
+const cardData: CardProps[] = [
+  {
+    label: "Total Revenue",
+    amount: "$45,231.89",
+    description: "+20.1% from last month",
+    icon: DollarSign
+  },
+  {
+    label: "Subscriptions",
+    amount: "+2350",
+    description: "+180.1% from last month",
+    icon: Users
+  },
+  {
+    label: "Sales",
+    amount: "+12,234",
+    description: "+19% from last month",
+    icon: CreditCard
+  },
+  {
+    label: "Active Now",
+    amount: "+573",
+    description: "+201 since last hour",
+    icon: Activity
+  }
+];
 
 export default function Home() {
   return (
--- components/Card.tsx
+++ components/Card.tsx
@@ -11,13 +11,24 @@ export type CardProps = {
 
 export default function Card(props: CardProps) {
   return (
-    <div>Card</div>
+    <CardContent>
+        <section className='flex justify-between gap-2'>
+            {/* label */}
+            <p className="text-sm">{props.label}</p>
+            {/* icon */}
+            <props.icon className='h-4 w-4 text-gray-400'/>
+        </section>
+        <section className='flex flex-col gap-1'>
+            <h2 className='text-2xl font-semibold'>{props.amount}</h2>
+            <p className='text-xs text-gray-500'>{props.description}</p>
+        </section>
+    </CardContent>
   )
 }
 
 export function CardContent(props: React.HTMLAttributes<HTMLDivElement>) {
     // In other words i ca decide to use a <p> tag inside this <div/>
     return (
-        <div {...props} className={cn("flex w-full flex-col gap-3 rounded-xl border p-5 shadow")}/>
+        <div {...props} className={cn("flex w-full flex-col gap-3 rounded-xl border p-5 shadow", props.className)}/>
     )
 }
