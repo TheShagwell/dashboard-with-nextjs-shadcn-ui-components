f93914ebcf8879c990c4007c6101fbcdf982dfd3
Erewharighe Ezra <kelvinerewharighe@gmail.com>
2025-12-17 13:27:59 +0100

    Improve code formatting and consistency in SideNavbar component

--- components/SideNavbar.tsx
+++ components/SideNavbar.tsx
@@ -1,78 +1,84 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react'
-
-import {
-    ChevronLeft,
-    ChevronRight,
-    LayoutDashboard,
-    Settings,
-    ShoppingCart,
-    UsersRound,
-  } from "lucide-react"
-import { Nav } from './ui/nav'
-import { Button } from './ui/button'
+import React, { useState } from "react";
 
 import {
-    useWindowWidth,
-  } from '@react-hook/window-size'
+  ChevronLeft,
+  ChevronRight,
+  LayoutDashboard,
+  Settings,
+  ShoppingCart,
+  UsersRound,
+} from "lucide-react";
+import { Nav } from "./ui/nav";
+import { Button } from "./ui/button";
 
-type Props = {}
+import { useWindowWidth } from "@react-hook/window-size";
+
+type Props = {};
 
 export default function SideNavbar({}: Props) {
-    const [isCollapsed, setIsCollapsed] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(false);
 
-    const onlyWidth = useWindowWidth();
-    const mobileWidth = onlyWidth < 768
+  const onlyWidth = useWindowWidth();
+  const mobileWidth = onlyWidth < 768;
 
-    function toggleSidebar(){
-        setIsCollapsed(!isCollapsed);
-    }
+  function toggleSidebar() {
+    setIsCollapsed(!isCollapsed);
+  }
 
   return (
-    <div className='relative min-w-[80px] border-r px-3 pb-10 pt-24'>
-        {!mobileWidth && (
-            <div className="absolute right-[-20px] top-7">
-                {isCollapsed ? (
-                  <Button onClick={toggleSidebar} variant="secondary" className='rounded-full p-2'>
-                      <ChevronRight/>
-                  </Button>
-                ) : 
-                  <Button onClick={toggleSidebar} variant="secondary" className='rounded-full p-2'>
-                    <ChevronLeft/>
-                  </Button>
-                }
-            </div>
-        )}
-        <Nav
-            isCollapsed={ mobileWidth ? true : isCollapsed}
-            links={[
-              {
-                title: "Dashboard",
-                href:"/",
-                icon: LayoutDashboard,
-                variant: "default",
-              },
-              {
-                title: "Users",
-                href:"/users",
-                icon: UsersRound,
-                variant: "ghost",
-              },
-              {
-                title: "Orders",
-                href:"/orders",
-                icon: ShoppingCart,
-                variant: "ghost",
-              },
-              {
-                title: "Settings",
-                href:"/settings",
-                icon: Settings,
-                variant: "ghost",
-              },
-            ]}
-          />
+    <div className="relative min-w-[80px] border-r px-3 pb-10 pt-24">
+      {!mobileWidth && (
+        <div className="absolute right-[-20px] top-7">
+          {isCollapsed ? (
+            <Button
+              onClick={toggleSidebar}
+              variant="secondary"
+              className="rounded-full p-2"
+            >
+              <ChevronRight />
+            </Button>
+          ) : (
+            <Button
+              onClick={toggleSidebar}
+              variant="secondary"
+              className="rounded-full p-2"
+            >
+              <ChevronLeft />
+            </Button>
+          )}
+        </div>
+      )}
+      <Nav
+        isCollapsed={mobileWidth ? true : isCollapsed}
+        links={[
+          {
+            title: "Dashboard",
+            href: "/",
+            icon: LayoutDashboard,
+            variant: "default",
+          },
+          {
+            title: "Users",
+            href: "/users",
+            icon: UsersRound,
+            variant: "ghost",
+          },
+          {
+            title: "Orders",
+            href: "/orders",
+            icon: ShoppingCart,
+            variant: "ghost",
+          },
+          {
+            title: "Settings",
+            href: "/settings",
+            icon: Settings,
+            variant: "ghost",
+          },
+        ]}
+      />
     </div>
-  )
+  );
 }
